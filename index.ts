0c3909a3e3ff05bb7ec4726749c4fe3479fa8379
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-05 23:28:18 +0700

    Khởi tạo dự án với giao diện

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import path from "path";
+
+const app = express();
+const port = 3000;
+
+// Thiết lập thư mục view và view engine pug
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug"); // Thiết lập Pug làm view engine
+
+// Thiết lập thư mục chứa file tĩnh
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("client/pages/home", {
+    pageTitle: "Trang chủ",
+  });
+});
+
+app.get('/admin/dashboard', (req: Request, res: Response) => {
+  res.render("admin/pages/dashboard", {
+    pageTitle: "Tổng quan"
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Website đang chạy trên cổng ${port}`);
+});
