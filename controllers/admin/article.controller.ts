07036b61c66e7a739f301bd1c9e1c39cf0751798
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-10 14:20:21 +0700

    4 : Làm trang thêm mới danh mục bài viết p1

---
+++ controllers/admin/article.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+export const category = (req: Request, res: Response) => {
+  res.render("admin/pages/article-category", {
+    pageTitle: "Quản lý danh mục bài viết"
+  });
+}
+
+export const createCategory = (req: Request, res: Response) => {
+  res.render("admin/pages/article-create-category", {
+    pageTitle: "Tạo danh mục bài viết"
+  });
+}
+
+export const createCategoryPost = (req: Request, res: Response) => {
+  console.log(req.body);
+
+  res.json({
+    code: "success",
+    message: "Tạo danh mục thành công!"
+  })
+}
