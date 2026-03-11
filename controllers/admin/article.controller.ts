d6ea9f79a0d1aaf60888bdd401a6f533dcd963f0
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-11 17:18:07 +0700

    10 : Làm trang chỉnh sửa danh mục bài viết

--- controllers/admin/article.controller.ts
+++ controllers/admin/article.controller.ts
@@ -3,6 +3,8 @@ import CategoryBlog from "../../models/categories-blog.model";
 import { buildCategoryTree } from "../../helpers/category.helper";
 import slugify from "slugify";
 
+import { pathAdmin } from '../../configs/variable.config';
+
 export const category = async (req: Request, res: Response) => {
   try {
     const recordList: any = await CategoryBlog.find({
@@ -74,3 +76,71 @@ export const createCategoryPost = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+export const editCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryList = await CategoryBlog.find({});
+
+    const categoryTree = buildCategoryTree(categoryList);
+
+    const id = req.params.id;
+
+    const categoryDetail = await CategoryBlog.findOne({
+      _id: id,
+      deleted: false
+    })
+
+    if(!categoryDetail) {
+      res.redirect(`/${pathAdmin}/article/category`);
+      return;
+    }
+
+    res.render("admin/pages/article-edit-category", {
+      pageTitle: "Chỉnh sửa danh mục bài viết",
+      categoryList: categoryTree,
+      categoryDetail: categoryDetail
+    });
+  } catch (error) {
+    res.redirect(`/${pathAdmin}/article/category`);
+  }
+}
+
+export const editCategoryPatch = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const existSlug = await CategoryBlog.findOne({
+      _id: { $ne: id }, // Loại trừ bản ghi có _id trùng với id truyền vào
+      slug: req.body.slug
+    })
+
+    if(existSlug) {
+      res.json({
+        code: "error",
+        message: "Đường dẫn đã tồn tại!"
+      })
+      return;
+    }
+
+    req.body.search = slugify(`${req.body.name}`, {
+      replacement: " ",
+      lower: true
+    });
+
+    await CategoryBlog.updateOne({
+      _id: id,
+      deleted: false
+    }, req.body)
+
+    res.json({
+      code: "success",
+      message: "Cập nhật thành công!"
+    })
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!"
+    })
+  }
+}
