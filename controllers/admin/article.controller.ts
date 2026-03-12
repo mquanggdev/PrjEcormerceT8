5487b8b4245eaa93ff2a96af6b0ae7671c46b014
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-12 15:17:02 +0700

    13 : Làm tính năng phân trang danh mục bài viết

--- controllers/admin/article.controller.ts
+++ controllers/admin/article.controller.ts
@@ -22,8 +22,28 @@ export const category = async (req: Request, res: Response) => {
       const keywordRegex = new RegExp(keyword, "i");
       find.search = keywordRegex;
     }
+    // Phân trang
+    const limitItems = 2;
+    let page = 1;
+    if (req.query.page && parseInt(`${req.query.page}`) > 0) {
+      page = parseInt(`${req.query.page}`);
+    }
+    const totalRecord = await CategoryBlog.countDocuments(find);
+    const totalPage = Math.ceil(totalRecord / limitItems);
+    const skip = (page - 1) * limitItems;
+    const pagination = {
+      totalRecord: totalRecord,
+      totalPage: totalPage,
+      skip: skip,
+    };
+    // Hết Phân trang
 
-    const recordList: any = await CategoryBlog.find(find);
+    const recordList: any = await CategoryBlog.find(find)
+      .sort({
+        createdAt: "desc",
+      })
+      .limit(limitItems)
+      .skip(skip);
 
     for (const item of recordList) {
       if (item.parent) {
@@ -38,6 +58,7 @@ export const category = async (req: Request, res: Response) => {
     res.render("admin/pages/article-category", {
       pageTitle: "Quản lý danh mục bài viết",
       recordList: recordList,
+      pagination: pagination
     });
   } catch (error) {
     console.log(error);
