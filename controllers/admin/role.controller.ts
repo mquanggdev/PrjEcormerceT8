1005ba80bea91ed1a79632ab773e18cd66612026
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-24 10:25:39 +0700

    36 : Tính năng xóa nhóm quyền

--- controllers/admin/role.controller.ts
+++ controllers/admin/role.controller.ts
@@ -152,3 +152,28 @@ export const editPatch = async (req: Request, res: Response) => {
     })
   }
 }
+
+
+export const deletePatch = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    await Role.updateOne({
+      _id: id
+    }, {
+      deleted: true,
+      deletedAt: Date.now(),
+    });
+
+    res.json({
+      code: "success",
+      message: "Xóa nhóm quyền thành công!"
+    })
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Id không hợp lệ!"
+    })
+  }
+}
