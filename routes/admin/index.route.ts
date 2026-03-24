c28073afeb86de7fdeabbea28c0f499958c6624f
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-24 18:16:56 +0700

    37 : Trang tạo tài khoản quản trị  - Bài 11 - PECM

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -5,6 +5,7 @@ import articleRoutes from "./article.route";
 import helperRoutes from "./helper.route";
 import fileManagerRoutes from "./file-manager.route";
 import roleRoutes from "./role.route";
+import accountAdminRoutes from "./account-admin.route";
 const router = Router();
 
 router.use('/dashboard', dashboardRoutes);
@@ -12,5 +13,6 @@ router.use('/article', articleRoutes);
 router.use('/helper', helperRoutes);
 router.use('/file-manager', fileManagerRoutes);
 router.use('/role', roleRoutes);
+router.use('/account-admin', accountAdminRoutes);
 
 export default router;
