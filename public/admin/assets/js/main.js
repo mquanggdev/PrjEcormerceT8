c9c899b4b48bb34a421097b6d93256070e2394d7
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-24 09:26:30 +0700

    32 : Sửa lỗi upload file trên TYMY MCE (Bài 10) - PECM

--- public/admin/assets/js/main.js
+++ public/admin/assets/js/main.js
@@ -3,45 +3,18 @@ const initialTinyMCE = () => {
   tinymce.init({
     selector: "[textarea-mce]",
     plugins: [
-      "anchor",
-      "autolink",
-      "charmap",
-      "codesample",
-      "emoticons",
-      "link",
-      "lists",
-      "media",
-      "searchreplace",
-      "table",
-      "visualblocks",
-      "wordcount",
-      "checklist",
-      "mediaembed",
-      "casechange",
-      "formatpainter",
-      "pageembed",
-      "a11ychecker",
-      "tinymcespellchecker",
-      "permanentpen",
-      "powerpaste",
-      "advtable",
-      "advcode",
-      "advtemplate",
-      "uploadcare",
-      "mentions",
-      "tableofcontents",
-      "footnotes",
-      "mergetags",
-      "autocorrect",
-      "typography",
-      "inlinecss",
-      "markdown",
-      "importword",
-      "exportword",
-      "exportpdf",
+      'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount', 'checklist', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'mentions', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf', 'image'
     ],
-    toolbar:
-      "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat | image',
+    init_instance_callback: (editor) => {
+      editor.on("OpenWindow", () => {
+        const title = document.querySelector(".tox .tox-dialog__title")?.innerHTML;
+        if(title == "Insert/Edit Media" || title == "Insert/Edit Image") {
+          const inputSource = document.querySelector(`.tox input.tox-textfield[type="url"]`);
+          inputSource.value = domainCDN;
+        }
+      })
+    }
   });
 };
 initialTinyMCE();
