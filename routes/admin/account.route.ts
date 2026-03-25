6af03aadde8679f3ae3ea16849f24105e52ab974
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-25 14:27:54 +0700

    43 : Bài 13 : Trang đăng nhập tài khoản bên Admin

---
+++ routes/admin/account.route.ts
@@ -0,0 +1,20 @@
+
+import { Router } from "express";
+import * as accountController from "../../controllers/admin/account.controller";
+import multer from "multer";
+import * as accountValidate from "../../validates/admin/account.validate";
+
+const router = Router();
+
+const upload = multer();
+
+router.get('/login', accountController.login);
+
+router.post(
+  '/login', 
+  upload.none(),
+  accountValidate.loginPost,
+  accountController.loginPost
+);
+
+export default router;
