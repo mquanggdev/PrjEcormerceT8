d4777d63f67803897a44dd3df95a44aa3161e92c
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-26 15:08:03 +0700

    52 : Tính năng sửa danh mục sản phẩm

--- controllers/admin/product.controller.ts
+++ controllers/admin/product.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import CategoryProduct from '../../models/category-product.model';
 import slugify from 'slugify';
 import { buildCategoryTree } from '../../helpers/category.helper';
+import { pathAdmin } from '../../configs/variable.config';
 
 export const category = async (req: Request, res: Response) => {
   const find: {
@@ -99,6 +100,74 @@ export const createCategoryPost = async (req: Request, res: Response) => {
     res.json({
       code: "success",
       message: "Tạo danh mục thành công!"
+    })
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!"
+    })
+  }
+}
+
+
+export const editCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryList = await CategoryProduct.find({});
+
+    const categoryTree = buildCategoryTree(categoryList);
+
+    const id = req.params.id;
+
+    const categoryDetail = await CategoryProduct.findOne({
+      _id: id,
+      deleted: false
+    })
+
+    if(!categoryDetail) {
+      res.redirect(`/${pathAdmin}/product/category`);
+      return;
+    }
+
+    res.render("admin/pages/product-edit-category", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      categoryList: categoryTree,
+      categoryDetail: categoryDetail
+    });
+  } catch (error) {
+    res.redirect(`/${pathAdmin}/product/category`);
+  }
+}
+
+export const editCategoryPatch = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const existSlug = await CategoryProduct.findOne({
+      _id: { $ne: id }, // Loại trừ bản ghi có _id trùng với id truyền vào
+      slug: req.body.slug
+    })
+
+    if(existSlug) {
+      res.json({
+        code: "error",
+        message: "Đường dẫn đã tồn tại!"
+      })
+      return;
+    }
+
+    req.body.search = slugify(`${req.body.name}`, {
+      replacement: " ",
+      lower: true
+    });
+
+    await CategoryProduct.updateOne({
+      _id: id,
+      deleted: false
+    }, req.body)
+
+    res.json({
+      code: "success",
+      message: "Cập nhật thành công!"
     })
   } catch (error) {
     res.json({
