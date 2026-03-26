dd273909b668bb5fb7b0a7121bdd84714f1d3731
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-26 15:04:04 +0700

    51 : Trang danh sách danh mục sản phẩm - PECm

--- controllers/admin/product.controller.ts
+++ controllers/admin/product.controller.ts
@@ -3,9 +3,63 @@ import CategoryProduct from '../../models/category-product.model';
 import slugify from 'slugify';
 import { buildCategoryTree } from '../../helpers/category.helper';
 
-export const category = (req: Request, res: Response) => {
+export const category = async (req: Request, res: Response) => {
+  const find: {
+    deleted: boolean,
+    search?: RegExp
+  } = {
+    deleted: false
+  };
+
+  if(req.query.keyword) {
+    const keyword = slugify(`${req.query.keyword}`, {
+      replacement: " ",
+      lower: true
+    });
+    const keywordRegex = new RegExp(keyword, "i");
+    find.search = keywordRegex;
+  }
+
+  // Phân trang
+  const limitItems = 20;
+  let page = 1;
+  if(req.query.page && parseInt(`${req.query.page}`) > 0) {
+    page = parseInt(`${req.query.page}`);
+  }
+  const totalRecord = await CategoryProduct.countDocuments(find);
+  const totalPage = Math.ceil(totalRecord/limitItems);
+  const skip = (page - 1) * limitItems;
+  const pagination = {
+    totalRecord: totalRecord,
+    totalPage: totalPage,
+    skip: skip
+  };
+  // Hết Phân trang
+
+  const recordList: any = await CategoryProduct
+    .find(find)
+    .sort({
+      createdAt: "desc"
+    })
+    .limit(limitItems)
+    .skip(skip)
+
+  for (const item of recordList) {
+    if(item.parent) {
+      const parent = await CategoryProduct.findOne({
+        _id: item.parent
+      })
+
+      item["parentName"] = parent?.name;
+    }
+  }
+
+
   res.render("admin/pages/product-category", {
-    pageTitle: "Quản lý danh mục sản phẩm"
+    pageTitle: "Quản lý danh mục sản phẩm",
+    recordList: recordList,
+    pagination: pagination
+
   });
 }
 
