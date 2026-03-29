0f68995cddad50d668ee5f3c2f58eba773162895
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-29 16:09:36 +0700

    86 Hiển thị thông tin ra Page Banner

--- controllers/client/product.controller.ts
+++ controllers/client/product.controller.ts
@@ -1,10 +1,22 @@
 
 import { Request, Response } from 'express';
+import CategoryProduct from '../../models/category-product.model';
 
-export const productByCategory = (req: Request, res: Response) => {
-  console.log(req.params.slug);
+export const productByCategory = async (req: Request, res: Response) => {
+  const categoryDetail = await CategoryProduct.findOne({
+    slug: req.params.slug,
+    deleted: false,
+    status: "active"
+  })
+
+  if(!categoryDetail) {
+    res.redirect("/");
+    return;
+  }
+
 
   res.render("client/pages/product-by-category", {
-    pageTitle: "Danh sách sản phẩm theo danh mục"
+    pageTitle: categoryDetail.name,
+    categoryDetail: categoryDetail
   });
 }
