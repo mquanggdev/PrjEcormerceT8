8493cc2821e76ea77bdd0ad480648addeffe7b40
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-29 16:04:20 +0700

    85 : Tạo Khung giao diện mẫu cho phần Sản Phẩm

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import homeRoutes from "./home.route";
 import articleRoutes from "./article.route";
 import * as categoryMiddleware from "../../middlewares/client/category.middleware";
+import productRoutes from "./product.route";
 
 const router = Router();
 router.use(categoryMiddleware.getAllCategory);
 
 router.use('/', homeRoutes);
 router.use('/article', articleRoutes);
+router.use('/product', productRoutes);
 
 export default router;
