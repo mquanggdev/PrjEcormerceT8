724e6c73a76f11c12e76b530efca5545ba964463
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-29 12:49:50 +0700

    77 : Hiển thị dữ liệu ra page bannner

--- controllers/client/article.controller.ts
+++ controllers/client/article.controller.ts
@@ -1,10 +1,23 @@
 
 import { Request, Response } from 'express';
 
-export const articleByCategory = (req: Request, res: Response) => {
-  console.log(req.params.slug);
+import CategoryBlog from '../../models/categories-blog.model';
+
+export const articleByCategory = async (req: Request, res: Response) => {
+    const categoryDetail = await CategoryBlog.findOne({
+    slug: req.params.slug,
+    deleted: false,
+    status: "active"
+  })
+
+  if(!categoryDetail) {
+    res.redirect("/");
+    return;
+  }
+
 
   res.render("client/pages/article-by-category", {
-    pageTitle: "Danh sách bài viết theo danh mục"
+    pageTitle: categoryDetail.name,
+    categoryDetail: categoryDetail
   });
 }
