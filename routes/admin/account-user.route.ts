ebb891d3eae5b58b7b2c1a8f87df292a11ccc8cd
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-01 09:59:13 +0700

    139 : Quản lý tài khoản user bên admin

---
+++ routes/admin/account-user.route.ts
@@ -0,0 +1,9 @@
+
+import { Router } from "express";
+import * as accountUserController from "../../controllers/admin/account-user.controller";
+
+const router = Router();
+
+router.get('/list', accountUserController.list);
+
+export default router;
