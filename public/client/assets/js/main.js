61434924b8215839827347fd43b92fb022acc546
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-11 16:52:09 +0700

    147 : Thanh toán bằng VNPay

--- public/client/assets/js/main.js
+++ public/client/assets/js/main.js
@@ -2554,6 +2554,10 @@ if(buttonOrder) {
               // Chuyển sang trang thanh toán bằng ZaloPay
               window.location.href = `/order/payment-zalopay?orderCode=${data.orderCode}&phone=${data.phone}`;
               break;
+            case "vnpay":
+              // Chuyển sang trang thanh toán bằng VNPay
+              window.location.href = `/order/payment-vnpay?orderCode=${data.orderCode}&phone=${data.phone}`;
+              break;
             default:
               window.location.href = "/";
               break;
