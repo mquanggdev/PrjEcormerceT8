fa7b12e9fc5f8b0dcf827f900dba2003fc0a2c94
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-18 16:39:40 +0700

    185 : Hiển thị dữ liệu ra

--- controllers/admin/block.controller.ts
+++ controllers/admin/block.controller.ts
@@ -1,8 +1,57 @@
 
 import { Request, Response } from "express";
+import path from "path";
+import fs from "fs";
+import slugify from "slugify";
+import Block from "../../models/block.model";
 
 export const list = async (req: Request, res: Response) => {
+  const recordList = await Block
+    .find({
+      deleted: false
+    })
+    .sort({
+      createdAt: "desc"
+    });
   res.render("admin/pages/block-list", {
-    pageTitle: "Quản lý block"
+    pageTitle: "Quản lý block",
+    recordList: recordList
+
   });
+}
+
+export const create = async (req: Request, res: Response) => {
+  // Lấy ra đường dẫn
+  const blocksDir = path.join(process.cwd(), "views", "client", "blocks"); // process.cwd() thư mục gốc
+  
+  // Lấy danh sách các file
+  const fileList = fs.readdirSync(blocksDir);
+
+  res.render("admin/pages/block-create", {
+    pageTitle: "Tạo block",
+    fileList: fileList
+  });
+}
+
+export const createPost = async (req: Request, res: Response) => {
+  try {
+    req.body.search = slugify(`${req.body.name} ${req.body.fileName}`, {
+      replacement: ' ',
+      lower: true,
+    })
+
+    const newRecord = new Block(req.body);
+    await newRecord.save();
+
+    res.json({
+      code: "success",
+      message: "Tạo block thành công!"
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!"
+    })
+  }
 }
--- routes/admin/block.route.ts
+++ routes/admin/block.route.ts
@@ -5,5 +5,7 @@ import * as blockController from "../../controllers/admin/block.controller";
 const router = Router();
 
 router.get('/list', blockController.list);
+router.get('/create', blockController.create);
+router.post('/create', blockController.createPost);
 
 export default router;
