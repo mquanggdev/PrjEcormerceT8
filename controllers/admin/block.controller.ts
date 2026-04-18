8093a0e9f242966b5c5461da9fb555d79fa8a0a1
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-18 16:34:10 +0700

    184 : Trang danh sách block - GD mẫu

---
+++ controllers/admin/block.controller.ts
@@ -0,0 +1,8 @@
+
+import { Request, Response } from "express";
+
+export const list = async (req: Request, res: Response) => {
+  res.render("admin/pages/block-list", {
+    pageTitle: "Quản lý block"
+  });
+}
---
+++ routes/admin/block.route.ts
@@ -0,0 +1,9 @@
+
+import { Router } from "express";
+import * as blockController from "../../controllers/admin/block.controller";
+
+const router = Router();
+
+router.get('/list', blockController.list);
+
+export default router;
