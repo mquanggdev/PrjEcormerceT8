8a5950d6b52a387f7a72497bcadf4299fed98e08
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-21 15:25:29 +0700

    194 : Chỉnh sửa nội dung Khối giao diện bên Admin

--- public/admin/assets/js/main.js
+++ public/admin/assets/js/main.js
@@ -47,6 +47,25 @@ const drawNotify = (type, message) => {
   }));
 }
 
+
+// Khởi tạo JSON Editor
+const jsonEditor = document.querySelector("[json-editor]");
+if(jsonEditor) {
+  const editor = new JSONEditor(jsonEditor, {
+    mode: "tree",
+    modes: ["text", "tree"],
+  })
+
+  // Dữ liệu mặc định
+  const data = jsonEditor.getAttribute("data");
+  editor.set(data ? JSON.parse(data) : {});
+
+  // Lưu editor vào đối tượng window để dùng khi submit
+  window.blockJsonEditor = editor;
+}
+// Hết Khởi tạo JSON Editor
+
+
 // articleCreateCategoryForm
 const articleCreateCategoryForm = document.querySelector("#articleCreateCategoryForm");
 if(articleCreateCategoryForm) {
@@ -2324,11 +2343,21 @@ if(blockCreateForm) {
       const name = event.target.name.value;
       const fileName = event.target.fileName.value;
       const status = event.target.status.value;
+      // Trường data
+      let dataObject = {};
+      try {
+        dataObject = window.blockJsonEditor.get();
+      } catch (error) {
+        notyf.error("Dữ liệu JSON không hợp lệ!");
+        return;
+      }
+      // Hết Trường data
 
       const dataFinal = {
         name: name,
         fileName: fileName,
-        status: status
+        status: status,
+        data : dataObject
       };
       
       fetch(`/${pathAdmin}/block/create`, {
@@ -2378,11 +2407,23 @@ if(blockEditForm) {
       const name = event.target.name.value;
       const fileName = event.target.fileName.value;
       const status = event.target.status.value;
+      
+
+      // Trường data
+      let dataObject = {};
+      try {
+        dataObject = window.blockJsonEditor.get();
+      } catch (error) {
+        notyf.error("Dữ liệu JSON không hợp lệ!");
+        return;
+      }
+      // Hết Trường data
 
       const dataFinal = {
         name: name,
         fileName: fileName,
-        status: status
+        status: status,
+        data : dataObject
       };
       
       fetch(`/${pathAdmin}/block/edit/${id}`, {
