bbbcc3a6c4043dc8628fd3a3fb7b4cd6a766332d
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-21 16:14:07 +0700

    199 : Khối Trending Product 2

--- helpers/product.helper.ts
+++ helpers/product.helper.ts
@@ -1,3 +1,5 @@
+import CategoryProduct from "../models/category-product.model";
+import Product from "../models/product.model";
 
 export const formatProductItem = (item: any) => {
   // Giảm giá
@@ -15,4 +17,49 @@ export const formatProductItem = (item: any) => {
       })
     })
   item.colorList = [...colorSet];
+}
+
+export const getProductByCategory = async (getByCategory: any) => {
+  let productList: any[] = [];
+
+  // Tạo đối tượng tìm kiếm
+  const find: any = {
+    deleted: false,
+    status: "active"
+  };
+
+  // Lấy mảng id các danh mục
+  if(getByCategory.category && getByCategory.category.length) {
+    const categoryList = await CategoryProduct.find({
+      slug: { $in: getByCategory.category },
+      deleted: false,
+      status: "active"
+    });
+    const categoryIds = categoryList.map((category: any) => category.id);
+    find.category = { $in: categoryIds };
+  }
+
+  // Lấy giới hạn sản phẩm
+  let limit = 10;
+  if(getByCategory.limit) {
+    limit = getByCategory.limit;
+  }
+
+  // Sắp xếp
+  const sort: any = {};
+  if(getByCategory.sort && getByCategory.sort.by && getByCategory.sort.type) {
+    sort[getByCategory.sort.by] = getByCategory.sort.type;
+  }
+
+  // Lấy ra sản phẩm
+  productList = await Product
+    .find(find)
+    .sort(sort)
+    .limit(limit);
+  
+  for(const item of productList) {
+    formatProductItem(item);
+  }
+
+  return productList;
 }
