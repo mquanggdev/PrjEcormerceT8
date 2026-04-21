1c3440bc316c052da85d86084af2b8a364682e79
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-21 13:12:25 +0700

    189 : Giao diện mẫu của template

---
+++ routes/admin/template.route.ts
@@ -0,0 +1,9 @@
+
+import { Router } from "express";
+import * as templateController from "../../controllers/admin/template.controller";
+
+const router = Router();
+
+router.get('/list', templateController.list);
+
+export default router;
