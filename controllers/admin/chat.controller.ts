4f906b8bbfa15dd69bda2398dcf7de2096fa7eb5
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-22 12:02:49 +0700

    206 : Giao diện mẫu bên admin

---
+++ controllers/admin/chat.controller.ts
@@ -0,0 +1,8 @@
+
+import { Request, Response } from 'express';
+
+export const myChatList = async (req: Request, res: Response) => {
+  res.render("admin/pages/my-chat-list", {
+    pageTitle: "Danh sách tin nhắn của bạn",
+  });
+}
