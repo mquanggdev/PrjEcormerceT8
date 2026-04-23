7f48b54b4f868f8d64953a2c807f821b59d16f4d
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-04-23 13:28:19 +0700

    212 : Chat bên admin

--- public/admin/assets/js/chat.js
+++ public/admin/assets/js/chat.js
@@ -2,7 +2,25 @@
 // Khởi tạo SocketIO bên Admin
 const socket = io();
 
-// Nhận tin nhắn từ server
-socket.on("SERVER_SEND_MESSAGE", (data) => {
-  console.log(data);
-});
+// Logic nhắn tin của Admin
+const formChat = document.querySelector("[form-chat]");
+if(formChat) {
+  const inputContent = formChat.querySelector("[input-content]");
+  const buttonSend = formChat.querySelector("[button-send]");
+
+  buttonSend.addEventListener("click", () => {
+    const content = inputContent.value.trim();
+    if(content) {
+      // Gửi tin nhắn lên cho server
+      socket.emit("CLIENT_SEND_MESSAGE", { // sự kiện client send message này tức là cả bên admin và client là bên người dùng , khác với sự kiện Server return lại tin nhắn là Server send message. 
+        content: content
+      });
+      inputContent.value = "";
+    }
+  })
+
+  // Nhận tin nhắn từ server
+  socket.on("SERVER_SEND_MESSAGE", (data) => {
+    console.log(data);
+  });
+}
--- sockets/chat.socket.ts
+++ sockets/chat.socket.ts
@@ -25,7 +25,12 @@ export const chatSocket = async (io: Server, socket: Socket) => {
         status: 'open'
       });
     }
+  }else if(account.role === 'admin') {
+      chatRoom = await ChatRoom.findOne({
+      adminId: account.id
+    });
   }
+  
 
   // Lắng nghe sự kiện CLIENT_SEND_MESSAGE
   socket.on('CLIENT_SEND_MESSAGE', async (data) => {
@@ -50,7 +55,17 @@ export const chatSocket = async (io: Server, socket: Socket) => {
           'unreadCount.admin': 1
         }
       })
+    }else if(account.role === 'admin') {
+      await ChatRoom.updateOne({
+        _id: chatRoom.id
+      }, {
+        $inc: {
+          'unreadCount.user': 1
+        }
+      })
     }
+ 
+
 
     // Phản hồi về cho tất cả mọi người
     io.emit('SERVER_SEND_MESSAGE', data);
